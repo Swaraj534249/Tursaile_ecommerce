c86a2b60b7a3cd43b03426c79e8663822fa8a83f
Swaraj Darne <swarajdarne.srj@gmail.com>
2025-02-05 15:08:10 +0530

    Delete unwanted files

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,14 +1,6 @@
 import {configureStore} from '@reduxjs/toolkit'
 import AuthSlice from '../features/auth/AuthSlice'
-import ProductSlice from '../features/products/ProductSlice'
 import UserSlice from '../features/user/UserSlice'
-import BrandSlice from '../features/brands/BrandSlice'
-import CategoriesSlice from '../features/categories/CategoriesSlice'
-import CartSlice from '../features/cart/CartSlice'
-import AddressSlice from '../features/address/AddressSlice'
-import ReviewSlice from '../features/review/ReviewSlice'
-import OrderSlice from '../features/order/OrderSlice'
-import WishlistSlice from '../features/wishlist/WishlistSlice'
 import VesselOwnerSlice from '../features/vesselOwner/VesselOwnerSlice'
 import VesselManagerSlice from '../features/vesselManager/VesselManagerSlice'
 import VesselSlice from '../features/vessel/VesselSlice'
@@ -20,15 +12,7 @@ import ProposeSlice from '../features/propose/ProposeSlice'
 export const store=configureStore({
     reducer:{
         AuthSlice,
-        ProductSlice,
         UserSlice,
-        BrandSlice,
-        CategoriesSlice,
-        CartSlice,
-        AddressSlice,
-        ReviewSlice,
-        OrderSlice,
-        WishlistSlice,
         VesselOwnerSlice,
         VesselManagerSlice,
         VesselSlice,
